696ae5d157b494a268dfb8bac7bff3e9bbea2a33
tanhwanYT <154482081+tanhwanYT@users.noreply.github.com>
2025-10-17 18:18:40 +0900

    Red Black Tree Learn

--- rbtree_lab/src/driver.c
+++ rbtree_lab/src/driver.c
@@ -1,5 +1,6 @@
 #include "rbtree.h"
 
 int main(int argc, char *argv[]) {
-
+    
 }
+
--- rbtree_lab/src/rbtree.c
+++ rbtree_lab/src/rbtree.c
@@ -4,18 +4,29 @@
 
 rbtree *new_rbtree(void) {
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
-  // TODO: initialize struct if needed
-  return p;
+  p->root->color=RBTREE_BLACK;
+  p->root->left=p->nil;
+  p->root->right=p->nil;  
+  return p->root;
 }
 
 void delete_rbtree(rbtree *t) {
-  // TODO: reclaim the tree nodes's memory
-  free(t);
+  if(t != t->nil)
+  {
+    delete_rbtree(&(t->root->left));
+    delete_rbtree(&(t->root->right));
+    free(t);
+    t=t->nil;
+  }
 }
 
 node_t *rbtree_insert(rbtree *t, const key_t key) {
-  // TODO: implement insert
-  return t->root;
+  node_t *newnode = (node_t *)calloc(1,sizeof(node_t));
+  newnode->key = key;
+  newnode->left=t->nil;
+  newnode->right=t->nil;
+  newnode->color=RBTREE_RED;  
+  return newnode;
 }
 
 node_t *rbtree_find(const rbtree *t, const key_t key) {
